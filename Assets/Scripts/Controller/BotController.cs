6d02d8aa727dfb3ce3e74ed7b7851dc20ddd7e83
Kirill <kirilloffk@gmail.com>
2025-04-23 15:47:56 +0500

    Update BotController.cs

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,9 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello
-//How are you?
-//i'm fine
+//hello Салют
+//How are you? Хорошо мая
+//i'm fine Твоя как поживает)
 namespace Controller
 {
     public class BotController
